4ce35567acdc05fa28d2fa78f0c539d1a884a77f
yusmnn <yusmanqq@gmail.com>
2023-07-06 03:08:01 +0800

    add: create navigation screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_twitter_clone_ui/view/screens/twitter_home/twitter_home_screen.dart';
+import 'package:flutter_twitter_clone_ui/view/screens/twitter_messages/twitter_messages_screen.dart';
+import 'package:flutter_twitter_clone_ui/view/screens/twitter_notifications/twitter_notif_screen.dart';
+import 'package:flutter_twitter_clone_ui/view/screens/twitter_trends/twitter_trends_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,55 +19,14 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      initialRoute: TwitterHomeScreen.routeName,
+      routes: {
+        TwitterHomeScreen.routeName: (context) => const TwitterHomeScreen(),
+        TwitterTrendsScreen.routeName: (context) => const TwitterTrendsScreen(),
+        TwitterNotifScreen.routeName: (context) => const TwitterNotifScreen(),
+        TwitterMessagesScreen.routeName: (context) =>
+            const TwitterMessagesScreen(),
+      },
     );
   }
 }
