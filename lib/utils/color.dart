c2b87e37a6e99d4f8dc5b8d6a6f462adc02675d6
yusmnn <yusmanqq@gmail.com>
2023-07-06 19:24:12 +0800

    add: const color

---
+++ lib/utils/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color bgColor = Color(0xFFFFFFFF);
+const Color cWhite = Color(0xffE7ECF0);
+const Color blueArchive = Color(0xff4C9EEB);
+const Color cGrey = Color(0xFF687684);
